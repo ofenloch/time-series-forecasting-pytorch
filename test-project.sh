7f63f39282a6b79b4f79b6a4d964a7993e428909
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2025-01-03 23:45:32 +0000

    update and improve test script

--- test-project.sh
+++ test-project.sh
@@ -7,30 +7,36 @@ OUTPUT_DIR="testoutput"
 source .venv/bin/activate
 
 ## declare an array variable
-declare -a SYMBOLS=("DIA" "DOGG" "GOOGL" "IBM" "TSLA" "WMT")
+declare -a SYMBOLS=("AAPL" "DIA" "DOGG" "GOOGL" "IBM" "TSLA" "WMT")
 
 ## loop through the above array
 for SYMBOL in "${SYMBOLS[@]}"
 do
-    # first run
+    echo " "
+    echo "Testing with symbol ${SYMBOL} ..."
+    echo " "
+    
+    echo "   first run"
     mkdir -p ${OUTPUT_DIR}/${SYMBOL}-run1
-    python project.py --symbol ${SYMBOL} --mode test &> project-run.log
+    python project.py --symbol "${SYMBOL}" --mode test --file "data/alphavantage_TIME_SERIES_DAILY_ADJUSTED__${SYMBOL}__data.json" &> project-run.log
     mv *.json ${OUTPUT_DIR}/${SYMBOL}-run1
     mv figure*.png ${OUTPUT_DIR}/${SYMBOL}-run1
     mv project-run.log ${OUTPUT_DIR}/${SYMBOL}-run1
 
-    # secont run
+    echo "   second run"
     mkdir -p ${OUTPUT_DIR}/${SYMBOL}-run2
-    python project.py --symbol ${SYMBOL} --mode test &> project-run.log
+    python project.py --symbol "${SYMBOL}" --mode test  --file "data/alphavantage_TIME_SERIES_DAILY_ADJUSTED__${SYMBOL}__data.json" &> project-run.log
     mv *.json ${OUTPUT_DIR}/${SYMBOL}-run2
     mv figure*.png ${OUTPUT_DIR}/${SYMBOL}-run2
     mv project-run.log ${OUTPUT_DIR}/${SYMBOL}-run2
 
     # there shouldn't be any differences if we run the script in test mode
+    echo "   diff'ing run1 and run2 ..."
     diff -r -q ${OUTPUT_DIR}/${SYMBOL}-run1 ${OUTPUT_DIR}/${SYMBOL}-run2
 
     # the new restults should be the same as the ones saved before
-    diff -r -q --exclude=figure*.png data/sanctioned-output/${SYMBOL} ${OUTPUT_DIR}/${SYMBOL}-run1
+    echo "   diff'ing sanctioned-output and run1 ..."
+    diff -r -q --exclude=figure*.png --exclude=00_alphavantage_TIME_SERIES_DAILY_ADJUSTED*.json data/sanctioned-output/${SYMBOL} ${OUTPUT_DIR}/${SYMBOL}-run1
 done
 
 
